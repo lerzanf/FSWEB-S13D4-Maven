5dd3d26debe55d383fd6557040934ce1fb8d1ad8
lerzanf <lerzanfirinciogullari@gmail.com>
2025-05-11 01:57:46 +0200

    Done

---
+++ src/main/java/org/example/Weapon.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public enum Weapon {
+    SWORD(20, 4);
+
+    private int damage;
+    private double attackSpeed;
+
+    Weapon(int damage, double attackSpeed) {
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
+
+}
+
+
